9463961329959e9bb25a416a18015f95b0319ade
stellababy2004 <stiliana.stoyanova@orange.fr>
2025-11-20 17:46:12 +0100

    Remove deleted files and stage modified file for clean repo

--- check_tables.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("instance/volunteers.db")
-cursor = conn.cursor()
-cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-tables = cursor.fetchall()
-print("Tables in database:", [t[0] for t in tables])
-conn.close()
