6341097581e7bb1c06dff39f486f157823c5e6f7
stellababy2004 <stiliana.stoyanova@orange.fr>
2025-11-25 11:12:43 +0100

    Fix import errors for production deployment - use backend.models imports

--- fix_role.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("instance/volunteers.db")
-cursor = conn.cursor()
-cursor.execute("UPDATE admin_users SET role = 'MODERATOR' WHERE role = 'moderator'")
-conn.commit()
-conn.close()
-print("Updated role values")
