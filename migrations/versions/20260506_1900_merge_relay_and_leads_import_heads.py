984e96a94e1d16dfeea5aa20619f07ac7a39946d
Stella Stoyanova <contact@helpchain.live>
2026-05-06 18:22:59 +0200

    Merge relay migration head

---
+++ migrations/versions/20260506_1900_merge_relay_and_leads_import_heads.py
@@ -0,0 +1,21 @@
+"""merge relay events and leads import heads
+
+Revision ID: 20260506_1900
+Revises: 20260506_1800, c2763e28aef9
+Create Date: 2026-05-06 19:00:00
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = "20260506_1900"
+down_revision = ("20260506_1800", "c2763e28aef9")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
